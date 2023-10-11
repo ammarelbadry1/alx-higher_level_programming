6957ffdfc079359fe1de95302aacf4a5b179a929
ammarelbadry1 <ammarelbadry1@gmail.com>
2023-10-11 18:05:32 +0300

    bug fix

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-from hidden_4 import *
+import hidden_4
 
 if __name__ == "__main__":
-    names_defined_module = dir(hidden_4)
-    filtered_names = (name for name in names_defined_module 
-                      if not name.startswith('__'))
-    for name in filtered_names:
-        print(name)
+    names = dir(hidden_4)
+    for name in names:
+        if (name[:2] != "__"):
+            print(name)
