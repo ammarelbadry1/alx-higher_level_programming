4d8cf99fedd18eb3e48d7d246d7319426efbeb2f
ammarelbadry1 <ammarelbadry1@gmail.com>
2024-01-31 23:21:58 +0200

    tasks upload

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,6 +1,3 @@
 #!/usr/bin/env bash
-# This script takes in a URL,
-# sends a request to that URL,
-# and displays the size of the body of the response
-
+# This script takes in a URL, sends a request to that URL, and displays the size of the body of the response
 curl -sI "$1" | grep -i content-length | awk '{print $2}'
--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,9 +1,3 @@
 #!/usr/bin/env bash
-# This script takes in a URL,
-# sends a GET request to the URL,
-# and displays the body of the response only if the status code is 200
-
-status=$(curl -sLI "$1" | grep '200 OK' | awk '{print $2}')
-if [ "$status" == 200 ]; then
-	curl -sL "$1"
-fi
+# This script takes in a URL, sends a GET request to the URL, and displays the body of the response only if the status code is 200
+curl -sL "$1"
