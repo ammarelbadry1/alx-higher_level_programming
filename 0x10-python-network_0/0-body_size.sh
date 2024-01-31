78aaa63cb45a16736d42bae38c6521faadc03e30
ammarelbadry1 <ammarelbadry1@gmail.com>
2024-01-31 23:11:51 +0200

    tasks upload

---
+++ 0x10-python-network_0/0-body_size.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+# This script takes in a URL,
+# sends a request to that URL,
+# and displays the size of the body of the response
+
+curl -sI $1 | grep -i content-length | awk '{print $2}'
